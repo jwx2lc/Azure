0e653586193f1c846afa54f5a26257a8f887ec44
Jonathan Whittenton <jpwhittenton@gmail.com>
2020-03-04 09:35:35 -0600

    Add database project and tables

---
+++ src/Power BI Data/Reporting/Reporting/Tables/Report.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Reporting].[Report] (
+    [ReportId] INT IDENTITY (1, 1) NOT NULL,
+    [DisplayName] NVARCHAR(50) NOT NULL, 
+    [ReportName] NVARCHAR(50) NOT NULL, 
+    [PowerBIReportId] UNIQUEIDENTIFIER NOT NULL, 
+    [PowerBIGroupId] UNIQUEIDENTIFIER NOT NULL, 
+    [CreateDate] DATETIME NOT NULL, 
+    [UpdateDate] DATETIME NULL, 
+    CONSTRAINT [PK_Report] PRIMARY KEY CLUSTERED ([ReportId] ASC)
+);
+
---
+++ src/Power BI Data/Reporting/Reporting/Tables/ReportRole.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Reporting].[ReportRole]
+(
+	[ReportRoleId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ReportId] INT NOT NULL, 
+    [RoleName] NVARCHAR(50) NOT NULL, 
+    [CreateDate] DATETIME NOT NULL, 
+    [UpdateDate] DATETIME NOT NULL, 
+    CONSTRAINT [FK_ReportRole_Report] FOREIGN KEY ([ReportId]) REFERENCES [Reporting].[Report]([ReportId])
+)
